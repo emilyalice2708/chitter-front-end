916aa74f175e204fef3a90407b41923ac056c60b
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-03 15:08:00 +0000

    Further tests added to check sign up error function

--- tests/peepView.test.js
+++ tests/peepView.test.js
@@ -17,8 +17,10 @@ describe('PeepView', () => {
     it('Adds peep list to document', () => {
       peepView.makeHTML(list)
       expect(container.innerHTML).toEqual(`<li>user1 posted:<br> peep <br>` +
+        ` Likes: 2 ` +
         `<a id=\"1\" href=\"#peeps/1\">View Peep</a></li>` +
         `<li>user1 posted:<br> peep 2 <br>` +
+        ` Likes: 2 ` +
         `<a id=\"2\" href=\"#peeps/2\">View Peep</a></li>`)
     })
   })
@@ -27,7 +29,7 @@ describe('PeepView', () => {
     it('Replaces list with single peep', () => {
       peepView.makeHTML(list)
       peepView.singlePeepHTML({body: "peep", user: {handle: "user1"}, id: "1", likes: [["first"], ["second"]]})
-      expect(container.innerHTML).toEqual("<li>user1 posted:<br> peep <br></li>")
+      expect(container.innerHTML).toEqual("<li>user1 posted:<br> peep <br> Likes: 2 </li>")
     })
   })
 })
