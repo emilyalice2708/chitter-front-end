91314fd2c3426411d6dfc71064ff832ba6c75619
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-04 12:27:32 +0000

    Switch let for const and add expectation variables

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -2,22 +2,16 @@ import { jest } from '@jest/globals';
 import UserController from '../models/userController';
 
 describe('signUp', () => {
-  let mockFetch;
-  let controller;
-  let container;
+  const mockFetch = jest.fn().mockResolvedValue({
+    json: () => {return {
+      id: 1,
+      handle: "newuser"
+    }}
+  })
+  const controller = new UserController();
 
   beforeEach(() => {
-    mockFetch = jest.fn().mockResolvedValue({
-      json: () => {return {
-        id: 1,
-        handle: "newuser"
-      }}
-    })
     global.fetch = mockFetch
-    controller = new UserController();
-    container = document.createElement("div");
-    container.setAttribute('id', 'welcome')
-    document.body.appendChild(container);
   });
 
   afterEach(() => {
@@ -26,11 +20,13 @@ describe('signUp', () => {
 
   describe('signUp', () => {
     it('calls fetch with the handle and password', async () => {
+      const dataBody = "{\"user\":{\"handle\":\"newuser\",\"password\":\"pword\"}}"
+      const url = "https://chitter-backend-api-v2.herokuapp.com/users"
       try {
         await controller.signUp("newuser", "pword");
 
-        expect(mockFetch.mock.calls[0][0]).toEqual("https://chitter-backend-api-v2.herokuapp.com/users")
-        expect(mockFetch.mock.calls[0][1].body).toEqual("{\"user\":{\"handle\":\"newuser\",\"password\":\"pword\"}}")
+        expect(mockFetch.mock.calls[0][0]).toEqual(url)
+        expect(mockFetch.mock.calls[0][1].body).toEqual(dataBody)
       } catch(error) {
         throw error;
       }
@@ -39,15 +35,17 @@ describe('signUp', () => {
 
   describe('completeSignUp', () => {
     it('displays welcome message if sign-up is successful', () => {
+      const container = document.createElement('div');
+      container.setAttribute('id', 'welcome')
+      document.body.appendChild(container);
       controller.completeSignUp({id: 1, handle: "newuser"})
-      let welcome = document.getElementById('welcome')
 
-      expect(welcome.innerHTML).toEqual("Welcome to Chitter, newuser. Sign in to post a peep!")
+      expect(container.innerHTML).toEqual("Welcome to Chitter, newuser. Sign in to post a peep!")
     })
 
     it('displays sign-up error to user if sign-up is unsuccessful', () => {
       controller.completeSignUp({id: 1, handle: "has already been taken"})
-      let error = document.getElementById('sign-up-error')
+      const error = document.getElementById('sign-up-error')
       expect(error.innerHTML).toEqual("That username is taken!")
     })
   })
