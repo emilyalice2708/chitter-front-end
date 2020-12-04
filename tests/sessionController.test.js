83636702427a8ea72e015cb741deae8670e83dae
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-04 12:21:54 +0000

    Switch let for const in sessionController test

--- tests/sessionController.test.js
+++ tests/sessionController.test.js
@@ -5,28 +5,22 @@ import PeepViewController from '../models/peepViewController.js';
 jest.mock('../models/peepViewController')
 
 describe('signIn', () => {
-  let mockFetch;
-  let controller;
-  let container;
-  let signedIn;
-  let mockPeepController;
+  const mockFetch = jest.fn().mockResolvedValue({
+    json: () => { return [{user: {handle: "newuser", password: "pword"}}] }
+  })
+  const controller = new SessionController();
+  const mockPeepController = new PeepViewController()
 
   beforeEach(() => {
-    mockFetch = jest.fn().mockResolvedValue({
-      json: () => { return [{user: {handle: "newuser", password: "pword"}}] }
-    })
     global.fetch = mockFetch
-    controller = new SessionController();
   });
 
   describe('#signUp', () => {
-    beforeEach(() => {
-      container = document.createElement("div");
-      container.setAttribute('id', 'sign-in-error')
-      document.body.appendChild(container);
-    })
 
     it('calls fetch to the correct url', async () => {
+      const container = document.createElement("div");
+      container.setAttribute('id', 'sign-in-error')
+      document.body.appendChild(container);
       try {
         await controller.signIn("newuser", "pword");
 
@@ -40,27 +34,26 @@ describe('signIn', () => {
   describe('#invalidSession', () => {
     it('displays a sign-in error to the user', () => {
       controller.invalidSession()
-      let error = document.getElementById('sign-in-error')
+      const error = document.getElementById('sign-in-error')
+      
       expect(error.innerHTML).toEqual('Incorrect user or password!')
     })
   })
 
   describe('#completeSignIn', () => {
-    beforeEach(() => {
-      signedIn = document.createElement("div")
+    it('displays logged-in message to user if sign-in is successful', () => {
+      const signedIn = document.createElement("div")
       signedIn.setAttribute('id', 'signed-in')
       document.body.appendChild(signedIn);
-      mockPeepController = new PeepViewController()
-    })
-
-    it('displays logged-in message to user if sign-in is successful', () => {
       controller.completeSignIn({id: 1, handle: "newuser"})
       let message = document.getElementById('signed-in')
+
       expect(message.innerHTML).toEqual("You're logged in. You can now post a peep!")
     })
 
     it('calls on PeepViewController to render post peeps form', () => {
       controller.completeSignIn({id: 1, handle: "newuser"})
+
       expect(mockPeepController.postPeep).toHaveBeenCalled
     })
   })
