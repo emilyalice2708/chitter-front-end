225ac0d13b624fd75f01fc8633486d4947456087
Emily Sesto <emilyalicesesto@gmail.com>
2020-12-04 12:04:25 +0000

    Create variable to store expectation in PeepView test

--- tests/peepView.test.js
+++ tests/peepView.test.js
@@ -7,7 +7,20 @@ describe('PeepView', () => {
 
   beforeEach(() => {
     peepView = new PeepView()
-    list = [{body: "peep", user: {handle: "user1"}, id: "1", likes: [["first"], ["second"]]}, {body: "peep 2", user: {handle: "user1"}, id: "2", likes: [["first"], ["second"]]}]
+    list = [{
+      body: "peep", user: {
+        handle: "user1"
+      }, 
+      id: "1", likes: [
+        ["first"], ["second"]]
+      }, {
+        body: "peep 2", 
+        user: {
+          handle: "user1"
+        }, 
+        id: "2", likes: [
+          ["first"], ["second"]]
+        }]
     container = document.createElement("div");
     container.setAttribute('id', 'peeps')
     document.body.appendChild(container);
@@ -15,13 +28,14 @@ describe('PeepView', () => {
 
   describe('#makeHTML', () => {
     it('Adds peep list to document', () => {
+      const htmlList = `<li>user1 posted:<br> peep <br>` +
+      ` Likes: 2 ` +
+      `<a id=\"1\" href=\"#peeps/1\">View Peep</a></li>` +
+      `<li>user1 posted:<br> peep 2 <br>` +
+      ` Likes: 2 ` +
+      `<a id=\"2\" href=\"#peeps/2\">View Peep</a></li>`
       peepView.makeHTML(list)
-      expect(container.innerHTML).toEqual(`<li>user1 posted:<br> peep <br>` +
-        ` Likes: 2 ` +
-        `<a id=\"1\" href=\"#peeps/1\">View Peep</a></li>` +
-        `<li>user1 posted:<br> peep 2 <br>` +
-        ` Likes: 2 ` +
-        `<a id=\"2\" href=\"#peeps/2\">View Peep</a></li>`)
+      expect(container.innerHTML).toEqual(htmlList)
     })
   })
 
